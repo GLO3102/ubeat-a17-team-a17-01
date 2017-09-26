e90c73f059bdf37d6c8316c6340890e571d8963b
Scrubby30 <felix0606@live.ca>
2017-09-26 04:55:48 -0400

    Création de la page artist + logo itunes dans les assets

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,9 +1,40 @@
 <template>
-    <div>
-        <h1>Artist</h1>
-        <div>GLO-3102 Artist page</div>
+    <div class="container">
+      <h3>Michael Jackson</h3>
+      <div class="divider"></div>
+      <div class="section">
+        <h5>Artist Information</h5>
+        <ul class="collection with-header">
+          <li class="collection-item">Full Name : Michael Jackson</li>
+          <li class="collection-item">Genre : Pop</li>
+        </ul>
+        <span>
+          ITunes Page Link :
+          <a target="_blank" href="https://itunes.apple.com/ca/artist/michael-jackson/id32940">
+            <img width="40" src="../assets/icons/ituneslogo.jpg" />
+          </a>
+        </span>
+      </div>
+      <div class="divider"></div>
+      <div class="section">
+        <h5 >Album List</h5>
+        <ul class="collection with-header">
+          <li class="collection-item">Got to Be There (1972)</li>
+          <li class="collection-item">Ben (1972)</li>
+          <li class="collection-item">Music and Me (1973)</li>
+          <li class="collection-item">forever, Michael (1975)</li>
+          <li class="collection-item">Off the Wall (1979)</li>
+          <li class="collection-item">Thriller (1982)</li>
+          <li class="collection-item">Bad (1987)</li>
+          <li class="collection-item">Dangerous (1991)</li>
+          <li class="collection-item">HIStory – Past, Present, and Future: Book 1 (1995)</li>
+          <li class="collection-item">Invincible</li>
+          <li class="collection-item">Michael (2010)</li>
+          <li class="collection-item">Xscape (2014)</li>
+        </ul>
+      </div>
     </div>
 </template>
 
-<style>
+<style src="@/assets/css/materialize.min.css">
 </style>
