38ef0365166543b586265553b295b158a8cc72ce
Rova <rova.randrian@gmail.com>
2017-09-30 17:29:32 -0400

    Design the navbar, change base html structure

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,91 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link to="/album">Album</router-link>
-            </li>
-            <li>
-                <router-link to="/artist">Artist</router-link>
-            </li>
+
+  <nav class="nav-extended">
+
+    <div class="nav-wrapper">
+
+      <router-link to="/" class="brand-logo left hide-on-small-only">UBeat</router-link>
+      <router-link to="/" class="brand-logo hide-on-med-and-up">UBeat</router-link>
+      <a href="#" data-activates="mobile-menu" class="button-collapse hide-on-med-only"><i class="material-icons">menu</i></a>
+
+      <ul id="nav-mobile" class="right hide-on-small-only">
+        <li>
+        <router-link to="/"><i class="material-icons left">home</i>Home</router-link>
+        </li>
+        <li>
+        <router-link to="/album"><i class="material-icons left">album</i>Album</router-link>
+        </li>
+        <li>
+        <router-link to="/artist"><i class="material-icons left">music_note</i>Artist</router-link>
+        </li>
+        <li class="hide-on-med-and-down">
+          <form>
+            <div class="input-field">
+              <input id="search" type="search" required>
+              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+              <i class="material-icons">close</i>
+            </div>
+          </form>
+        </li>
+        <li>
+          <i class="material-icons left">account_circle</i>MJ Fan
+        </li>
+        <li>
+          <a class="dropdown-button" href="#!" data-activates="dropdown-account-settings"><i class="material-icons right">arrow_drop_down</i>
+          </a>
+        </li>
+
+        <ul id="dropdown-account-settings" class="dropdown-content">
+          <li>
+            <router-link to=""><i class="material-icons left">account_box</i>Settings</router-link>
+          </li>
+          <li>
+            <router-link to=""><i class="material-icons left">exit_to_app</i>Logout</router-link>
+          </li>
         </ul>
-    </nav>
+
+      </ul>
+
+      <ul class="side-nav" id="mobile-menu">
+        <li>
+          <router-link to=""><i class="material-icons left">account_circle</i>MJ Fan</router-link>
+        </li>
+        <li>
+          <router-link to="/"><i class="material-icons left">home</i>Home</router-link>
+        </li>
+        <li>
+          <router-link to="/album"><i class="material-icons left">album</i>Album</router-link>
+        </li>
+        <li>
+          <router-link to="/artist"><i class="material-icons left">music_note</i>Artist</router-link>
+        </li>
+        <li>
+          <router-link to=""><i class="material-icons left">account_box</i>Settings</router-link>
+        </li>
+        <li>
+          <router-link to=""><i class="material-icons left">exit_to_app</i>Logout</router-link>
+        </li>
+      </ul>
+
+    </div>
+
+    <div class="nav-wrapper hide-on-large-only">
+      <div id="searchbar">
+        <form>
+          <div class="input-field">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </div>
+
+  </nav>
+
 </template>
 
-<style>
+<style src="@/assets/css/nav.css">
 </style>
+<script type="text/javascript" src="@/assets/js/nav.js">
+</script>
