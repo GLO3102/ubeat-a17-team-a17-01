5822366b5c57fb64a8c682db29ed6487218ca50e
Angélique Nguyen <angelique.nguyen@eisti.fr>
2017-10-01 18:21:47 -0400

    Resolve bug : script executed when page changes

--- src/assets/js/album.js
+++ src/assets/js/album.js
@@ -1,33 +1,46 @@
-$( window ).on('load', function(){
-  let tracks = $('.track');
+export default {
+  mounted() {
+    const tracks = document.getElementsByClassName('track');
+    let playingTrackIndex = -1;
+    if (screen.width >= 992) {
+      for (let i = 0; i < tracks.length; i += 1) {
+        tracks[i].addEventListener('mouseover', function displayPlayIcon() {
+          this.querySelector('.track-number').style.display = 'none';
+          this.querySelector('.play-pause-icon').style.display = 'block';
+        });
 
-  if (screen.width >= 992) {
-    tracks.on('mouseover', function () {
-      $(this).find('.track-number').hide();
-      $(this).find('.play-pause-icon').show();
-    });
-
-    tracks.on('mouseout', function () {
-      $(this).find('.track-number').show();
-      $(this).find('.play-pause-icon').text('play_circle_outline');
-      $(this).find('.play-pause-icon').hide();
-    });
-    tracks.on('click', function () {
-      if ($(this).find('.play-pause-icon').text() === 'play_circle_outline') {
-        $(this).find('.play-pause-icon').text('pause_circle_outline');
-      } else {
-        $(this).find('.play-pause-icon').text('play_circle_outline');
+        tracks[i].addEventListener('mouseout', function hidePlayIcon() {
+          if (this.querySelector('.play-pause-icon').innerHTML !== 'pause_circle_outline') {
+            this.querySelector('.track-number').style.display = 'block';
+            this.querySelector('.play-pause-icon').style.display = 'none';
+          }
+        });
+        tracks[i].addEventListener('click', function playPause() {
+          if (this.querySelector('.play-pause-icon').innerHTML === 'play_circle_outline') {
+            if (playingTrackIndex !== -1) {
+              tracks[playingTrackIndex].querySelector('.play-pause-icon').innerHTML = 'play_circle_outline';
+              tracks[playingTrackIndex].querySelector('.play-pause-icon').style.display = 'none';
+              tracks[playingTrackIndex].querySelector('.track-number').style.display = 'block';
+            }
+            this.querySelector('.play-pause-icon').innerHTML = 'pause_circle_outline';
+            playingTrackIndex = i;
+          } else {
+            this.querySelector('.play-pause-icon').innerHTML = 'play_circle_outline';
+            playingTrackIndex = -1;
+          }
+        });
       }
-    });
-  }
-  else {
-    tracks.on('click', function () {
-      if ($(this).find('.play-icon-responsive').text() === 'play_circle_outline') {
-        tracks.find('.play-icon-responsive').text('play_circle_outline');
-        $(this).find('.play-icon-responsive').text('pause_circle_outline');
-      } else {
-        $(this).find('.play-icon-responsive').text('play_circle_outline');
+    } else {
+      for (let i = 0; i < tracks.length; i += 1) {
+        tracks[i].addEventListener('click', function MouseClickResponsive() {
+          if (this.querySelector('.play-icon-responsive').innerHTML === 'play_circle_outline') {
+            tracks.querySelector('.play-icon-responsive').innerHTML = 'play_circle_outline';
+            this.querySelector('.play-icon-responsive').innerHTML = 'pause_circle_outline';
+          } else {
+            this.querySelector('.play-icon-responsive').innerHTML = 'play_circle_outline';
+          }
+        });
       }
-    });
+    }
   }
-});
+};
