b185f8bcbd237517daadf2c92fa6bf6e705853e0
Angélique Nguyen <angelique.nguyen@eisti.fr>
2017-09-30 20:37:10 -0400

    Add album page

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,9 +1,97 @@
 <template>
-    <div>
-        <h1>Album</h1>
-        <div>GLO-3102 Album page</div>
+  <div class="container">
+    <div class="row playlist-information">
+      <div class="album-sub-information">
+        <h3>Bad</h3>
+        <h5>Michael Jackson</h5>
+        <div class="playlist-description">
+          <p>Genre : Pop</p>
+          <p>Released : Aug 31, 1987</p>
+          <span>View in
+            <a class="waves-effect waves-light" target="_blank" href="https://itunes.apple.com/us/album/bad/id559334659">
+              <img width="62" src="../assets/icons/itunes-logo.png" />
+            </a>
+          </span>
+          <p>11 songs</p>
+        </div>
+      </div>
+      <img class="responsive-img playlist-img" src="../assets/img/album-bad.jpg">
     </div>
+
+    <ul class="collection tracks-list">
+      <li class="collection-item avatar">
+        <span class="title">Title</span>
+        <p class="secondary-content track-time">Time</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">1</p>
+        <span class="title">Bad</span>
+        <p class="secondary-content track-time">4:07</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">2</p>
+        <span class="title">The Way You Make Me Feel</span>
+        <p class="secondary-content track-time">4:58</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">3</p>
+        <span class="title">Speed Demon</span>
+        <p class="secondary-content track-time">4:02</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">4</p>
+        <span class="title">Liberian Girl</span>
+        <p class="secondary-content track-time">3:52</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">5</p>
+        <span class="title">Just Good Friends</span>
+        <p class="secondary-content track-time">4:07</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">6</p>
+        <span class="title">Another Part of Me</span>
+        <p class="secondary-content track-time">3:54</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">7</p>
+        <span class="title">Man In the Mirror</span>
+        <p class="secondary-content track-time">5:18</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">8</p>
+        <span class="title">I Just Can't Stop Loving You</span>
+        <p class="secondary-content track-time">4:13</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">9</p>
+        <span class="title">Dirty Diana</span>
+        <p class="secondary-content track-time">4:40</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">10</p>
+        <span class="title">Smooth Criminal</span>
+        <p class="secondary-content track-time">4:17</p>
+      </li>
+      <li class="collection-item avatar track">
+        <i class="material-icons play-pause-icon">play_circle_outline</i>
+        <p class="track-number">11</p>
+        <span class="title">Leave Me Alone</span>
+        <p class="secondary-content track-time">4:40</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style>
-</style>
+<style src="@/assets/css/playlist.css"></style>
+<script type="text/javascript" src="../assets/js/album.js"></script>
--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="container">
-      <h3>Michael Jackson</h3>
-      <div class="divider"></div>
-      <div class="section">
-        <h5>Artist Information</h5>
-        <ul class="collection with-header">
-          <li class="collection-item">Full Name : Michael Jackson</li>
-          <li class="collection-item">Genre : Pop</li>
-        </ul>
-        <span>
-          ITunes Page Link :
-          <a target="_blank" href="https://itunes.apple.com/ca/artist/michael-jackson/id32940">
-            <img width="2%" src="../assets/icons/ituneslogo.jpg" />
-          </a>
-        </span>
+      <div class="row playlist-information">
+        <div>
+          <h3>Michael Jackson</h3>
+          <div class="playlist-description">
+            <p>Full Name : Michael Joseph Jackson</p>
+            <p>Genre : Pop</p>
+            <span>View in
+            <a class="waves-effect waves-light" target="_blank" href="https://itunes.apple.com/ca/artist/michael-jackson/id32940">
+              <img width="62" src="../assets/icons/itunes-logo.png" />
+            </a>
+          </span>
+          </div>
+        </div>
+        <img class="responsive-img playlist-img" src="../assets/img/michael-jackson.jpg">
       </div>
-      <div class="divider"></div>
+
       <div class="section">
         <h5 >Album List</h5>
         <ul class="collection with-header">
@@ -35,3 +35,5 @@
       </div>
     </div>
 </template>
+
+<style src="@/assets/css/playlist.css"></style>
