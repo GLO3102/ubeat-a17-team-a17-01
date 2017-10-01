f00d9d0b55e87e14f616453a65138b1e07a0bb26
Angélique Nguyen <angelique.nguyen@eisti.fr>
2017-09-30 21:46:19 -0400

    Add play and pause buttons for tablets and mobiles on album page

--- src/assets/js/album.js
+++ src/assets/js/album.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
   let tracks = $('.track');
 
-  tracks.on('mouseover', function () {
-    $(this).find('.track-number').hide();
-    $(this).find('.play-pause-icon').show();
-  });
+  if (screen.width >= 992) {
+    tracks.on('mouseover', function () {
+      $(this).find('.track-number').hide();
+      $(this).find('.play-pause-icon').show();
+    });
 
-  tracks.on('mouseout', function () {
-    $(this).find('.track-number').show();
-    $(this).find('.play-pause-icon').text('play_circle_outline');
-    $(this).find('.play-pause-icon').hide();
-  });
-
-  tracks.on('click', function () {
-    if ($(this).find('.play-pause-icon').text() === 'play_circle_outline') {
-      $(this).find('.play-pause-icon').text('pause_circle_outline');
-    } else {
+    tracks.on('mouseout', function () {
+      $(this).find('.track-number').show();
       $(this).find('.play-pause-icon').text('play_circle_outline');
-    }
-  });
+      $(this).find('.play-pause-icon').hide();
+    });
+    tracks.on('click', function () {
+      if ($(this).find('.play-pause-icon').text() === 'play_circle_outline') {
+        $(this).find('.play-pause-icon').text('pause_circle_outline');
+      } else {
+        $(this).find('.play-pause-icon').text('play_circle_outline');
+      }
+    });
+  }
+  else {
+    tracks.on('click', function () {
+      if ($(this).find('.play-icon-responsive').text() === 'play_circle_outline') {
+        tracks.find('.play-icon-responsive').text('play_circle_outline');
+        $(this).find('.play-icon-responsive').text('pause_circle_outline');
+      } else {
+        $(this).find('.play-icon-responsive').text('play_circle_outline');
+      }
+    });
+  }
 });
