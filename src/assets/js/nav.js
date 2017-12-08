857dbbd244b96072308c95189c68a1229372c3b6
Scrubby30 <felix0606@live.ca>
2017-12-07 20:35:16 -0500

    Search by nav bar
    Search by navigation bar is possible with the autocomplete.

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,4 +1,46 @@
+import * as Cookies from 'js-cookie';
+import Autocomplete from 'vue2-autocomplete-js';
+import * as api from './api';
+
+
 $( window ).on('load', function(){
   $(".button-collapse").sideNav();
   $(".dropdown-button").dropdown();
 });
+
+export default {
+  components: {
+    navsearchbar: Autocomplete
+  },
+  data: () => ({
+    ownerEmail: 'userteam1@team1.com',
+    searchUrl: `${api.baseUrl}/search/`,
+    token: Cookies.get('token')
+  }),
+
+  methods: {
+    processJSON(json) {
+      const result = json.results.slice(0, 5);
+      const arrayLength = result.length;
+
+      for (let i = 0; i < arrayLength; i++) {
+        if (result[i].wrapperType === 'artist') {
+          result[i].name = result[i].artistName;
+        } else if (result[i].wrapperType === 'track') {
+          result[i].name = result[i].trackName;
+        } else {
+          result[i].name = result[i].collectionName;
+          result[i].wrapperType = 'album';
+        }
+      }
+      return result;
+    },
+    selectPage(result) {
+      if (result.wrapperType === 'artist') {
+        this.$router.push({ path: `/artist/${result.artistId}` });
+      } else {
+        this.$router.push({ path: `/album/${result.collectionId}` });
+      }
+    }
+  }
+};
